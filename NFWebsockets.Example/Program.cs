88389409c3f0d69a26c5e129e7b7900c4901d15f
José Simões <jose.simoes@eclo.solutions>
2021-03-27 21:14:09 +0000

    Improvements in network start

--- NFWebsockets.Example/Program.cs
+++ NFWebsockets.Example/Program.cs
@@ -1,6 +1,7 @@
 using nanoframework.System.Net.Websockets;
 using nanoframework.System.Net.Websockets.Client;
 using nanoframework.System.Net.Websockets.Server;
+using nanoFramework.Networking;
 using System;
 using System.Collections;
 using System.Diagnostics;
@@ -19,35 +20,30 @@ namespace NFWebsockets.Example
             //Nanoframework makes it easy to create a websocket server. And to create a new WebSocket Client. Here's a quick example of how easy.
             //use the URL provided in the debug output and connect your own client. Or use your device to connect to your websocket server in the cloud.   
 
+            NetworkHelpers.SetupAndConnectNetwork(false);
 
-            string ip = CheckIP();
-            //making sure your NFdevice has a network connection
-            if (ip != string.Empty)
+            Debug.WriteLine("Waiting for network up and IP address...");
+
+            NetworkHelpers.IpAddressAvailable.WaitOne();
+
+            //Lets create a new default webserver.
+            //WebSocketServer webSocketServer = new WebSocketServer();
+            //webSocketServer.Start();
+            //Let's echo all incomming messages from clients to all connected clients including the sender. 
+            //webSocketServer.MessageReceived += WebSocketServer_MesageReceived;
+            //Debug.WriteLine($"Websocket server is up and running, connect on: ws://{ip}:{webSocketServer.Port}{webSocketServer.Prefix}");
+
+            //Now let's also attach a local websocket client. Just because we can :-)
+            WebSocketClient client = new WebSocketClient();
+
+            //connect to the local client and write the messages to the debug output
+            client.Connect("ws://echo.websocket.org", Client_MessageReceived);
+
+            //While the client is connected will send a friendly hello every second.
+            while (client.State == nanoframework.System.Net.Websockets.WebSocketFrame.WebSocketState.Open)
             {
-
-                //Lets create a new default webserver.
-                //WebSocketServer webSocketServer = new WebSocketServer();
-                //webSocketServer.Start();
-                //Let's echo all incomming messages from clients to all connected clients including the sender. 
-                //webSocketServer.MessageReceived += WebSocketServer_MesageReceived;
-                //Debug.WriteLine($"Websocket server is up and running, connect on: ws://{ip}:{webSocketServer.Port}{webSocketServer.Prefix}");
-
-                //Now let's also attach a local websocket client. Just because we can :-)
-                WebSocketClient client = new WebSocketClient();
-
-                //connect to the local client and write the messages to the debug output
-                client.Connect("wss://echo.websocket.org", Client_MessageReceived);
-
-                //While the client is connected will send a friendly hello every second.
-                while (client.State == nanoframework.System.Net.Websockets.WebSocketFrame.WebSocketState.Open)
-                {
-                    client.SendString("hello");
-                    Thread.Sleep(1000);
-                }
-
-                // Browse our samples repository: https://github.com/nanoframework/samples
-                // Check our documentation online: https://docs.nanoframework.net/
-                // Join our lively Discord community: https://discord.gg/gCyBu8T
+                client.SendString("hello");
+                Thread.Sleep(1000);
             }
 
             Thread.Sleep(Timeout.Infinite);
@@ -83,24 +79,6 @@ namespace NFWebsockets.Example
                 {
                     webSocketServer.BroadCast(buffer);
                 }
-            }
-
-            private static string CheckIP()
-            {
-                Debug.WriteLine("Checking for IP");
-
-                NetworkInterface ni = NetworkInterface.GetAllNetworkInterfaces()[0];
-                if (ni.IPv4Address != null && ni.IPv4Address.Length > 0)
-                {
-                    if (ni.IPv4Address[0] != '0')
-                    {
-                        Debug.WriteLine($"We have and IP: {ni.IPv4Address}");
-                        return ni.IPv4Address;
-
-                    }
-                }
-
-                return string.Empty;
             }
         }
     }
