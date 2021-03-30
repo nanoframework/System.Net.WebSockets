1f8b949312cc97c8bebacb33b33493cbdf82a08a
José Simões <jose.simoes@eclo.solutions>
2021-03-30 18:11:54 +0100

    Improvements in sample app
    - Rework received messages handler.
    - Minor rework on network helpers.

--- NFWebsockets.Example/Program.cs
+++ NFWebsockets.Example/Program.cs
@@ -20,11 +20,15 @@ namespace NFWebsockets.Example
             //Nanoframework makes it easy to create a websocket server. And to create a new WebSocket Client. Here's a quick example of how easy.
             //use the URL provided in the debug output and connect your own client. Or use your device to connect to your websocket server in the cloud.   
 
-            NetworkHelpers.SetupAndConnectNetwork(false);
+            NetworkHelpers.SetupAndConnectNetwork(true);
 
             Debug.WriteLine("Waiting for network up and IP address...");
-
             NetworkHelpers.IpAddressAvailable.WaitOne();
+
+            Debug.WriteLine("Waiting for valid date&time...");
+            NetworkHelpers.DateTimeAvailable.WaitOne();
+
+            Debug.WriteLine("All set!");
 
             //Lets create a new default webserver.
             //WebSocketServer webSocketServer = new WebSocketServer();
@@ -37,13 +41,18 @@ namespace NFWebsockets.Example
             WebSocketClient client = new WebSocketClient();
 
             //connect to the local client and write the messages to the debug output
-            client.Connect("ws://echo.websocket.org", Client_MessageReceived);
+            client.Connect("wss://echo.websocket.org", Client_MessageReceived);
+
+            int helloCounter = 0;
 
             //While the client is connected will send a friendly hello every second.
             while (client.State == nanoframework.System.Net.Websockets.WebSocketFrame.WebSocketState.Open)
             {
-                client.SendString("hello");
-                Thread.Sleep(1000);
+                Debug.WriteLine($"About to send: {helloCounter}");
+
+                client.SendString($"hello {helloCounter++}");
+
+                Thread.Sleep(500);
             }
 
             Thread.Sleep(Timeout.Infinite);
@@ -51,35 +60,31 @@ namespace NFWebsockets.Example
 
         private static void Client_MessageReceived(object sender, MessageReceivedEventArgs e)
         {
-            Byte[] buffer = new Byte[e.Frame.MessageLength];
-            e.Frame.MessageStream.Read(buffer, 0, buffer.Length);
             if (e.Frame.MessageType == nanoframework.System.Net.Websockets.WebSocketFrame.WebSocketMessageType.Text)
             {
-                Debug.WriteLine($"received websocket message: {Encoding.UTF8.GetString(buffer, 0, buffer.Length)}");
-
+                Debug.WriteLine($"websocket message: {Encoding.UTF8.GetString(e.Frame.Buffer, 0, e.Frame.Buffer.Length)}");
             }
-            else 
+            else
             {
-                Debug.WriteLine("received websocket data");
+                Debug.WriteLine("websocket binary data");
             }
         }
 
         private static void WebSocketServer_MesageReceived(object sender, MessageReceivedEventArgs e)
-            {
-            
-                var webSocketServer = (WebSocketServer)sender;
-                Byte[] buffer = new Byte[e.Frame.MessageLength];
-                e.Frame.MessageStream.Read(buffer, 0, buffer.Length);
-                if (e.Frame.MessageType == nanoframework.System.Net.Websockets.WebSocketFrame.WebSocketMessageType.Text)
-                {
-                    webSocketServer.BroadCast(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        {
 
-                }
-                else 
-                {
-                    webSocketServer.BroadCast(buffer);
-                }
-            }
+            //var webSocketServer = (WebSocketServer)sender;
+            //Byte[] buffer = new Byte[e.Frame.MessageLength];
+            //e.Frame.MessageStream.Read(buffer, 0, buffer.Length);
+            //if (e.Frame.MessageType == nanoframework.System.Net.Websockets.WebSocketFrame.WebSocketMessageType.Text)
+            //{
+            //    webSocketServer.BroadCast(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+
+            //}
+            //else
+            //{
+            //    webSocketServer.BroadCast(buffer);
+            //}
         }
     }
-
+}
